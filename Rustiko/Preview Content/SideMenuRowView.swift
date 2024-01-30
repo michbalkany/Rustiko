6eb0c97759df7cf1a82f37edce51a8b75456b413
mich balkany <askmitchb@gmail.com>
2024-01-29 23:35:26 -0500

    populated sideMenuView with individual options to navigate through app, SideMenuOptionsModel created to store the data populating the side menu,

---
+++ Rustiko/Preview Content/SideMenuRowView.swift
@@ -0,0 +1,18 @@
+//
+//  SideMenuRowView.swift
+//  Rustiko
+//
+//  Created by Mich balkany on 1/29/24.
+//
+
+import SwiftUI
+
+struct SideMenuRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SideMenuRowView()
+}
---
+++ Rustiko/SideMenuOptionsModel.swift
@@ -0,0 +1,9 @@
+//
+//  SideMenuOptions.swift
+//  Rustiko
+//
+//  Created by Mich balkany on 1/29/24.
+//
+
+import Foundation
+
