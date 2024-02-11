cd077fd4a449e02f02c4eb1fc138d38d3f60f645
mich balkany <askmitchb@gmail.com>
2024-02-11 11:02:27 -0500

    order view, delivery view, and pickup view, views created

---
+++ Rustiko/Order Views/DeliveryView.swift
@@ -0,0 +1,24 @@
+//
+//  DeliveryView.swift
+//  Rustiko
+//
+//  Created by Mich balkany on 2/11/24.
+//
+
+import SwiftUI
+
+struct DeliveryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        // pop-over Address/apt for with dropdown
+        //save button
+        // apply button
+        
+        // delivery time drop down with dates and times
+        // order delivery button segway to menu
+    }
+}
+
+#Preview {
+    DeliveryView()
+}
---
+++ Rustiko/Order Views/PickUpView.swift
@@ -0,0 +1,21 @@
+//
+//  PickUpView.swift
+//  Rustiko
+//
+//  Created by Mich balkany on 2/11/24.
+//
+
+import SwiftUI
+
+struct PickUpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+    // drop down for pick time
+    // order pickup button that segways to menu
+    }
+}
+
+#Preview {
+    PickUpView()
+}
