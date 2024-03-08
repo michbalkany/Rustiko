99b790313cd0c6a94b6e1aa067d5083aeac634a2
Micah Njeru <micahnjeru1@gmail.com>
2024-03-08 12:53:54 +0300

    Edited the scrollview, got the scrollview button to work

--- Rustiko/Data/Struct & Codable/ScrollView Prototype/ScrollViewV1.swift
+++ Rustiko/Data/Struct & Codable/ScrollView Prototype/ScrollViewV1.swift
@@ -16,61 +16,82 @@ struct ScrollViewV1: View {
         dinner,
         lunch
     ]
-
+    
     // Test menu
     let menu: [Menu]
     
     var body: some View {
         VStack {
             // Top ScrollView
-            ScrollViewReader { value in
-                ScrollView {
-                    Button("Jump to #5") {
-                        value.scrollTo(5, anchor: .top)
-                    }
-                    .padding()
-                    
-                    ForEach(menu, id: \.self) { item in
-                        Text(item.category)
-                            .font(.title)
-                            .frame(width: 200, height: 200)
-                            .id(item.id)
-                    }
-                }
-            }
+            //            ScrollViewReader { value in
+            //                ScrollView {
+            //                    HStack {
+            //                        Button("Jump to #1") {
+            //                            value.scrollTo(1, anchor: .top)
+            //                        }
+            //
+            //                        Button("Jump to #5") {
+            //                            value.scrollTo(7)
+            //                        }
+            //                    }
+            //
+            //                    .padding()
+            //
+            //                    ForEach(menu, id: \.self) { item in
+            //                        Text(item.category)
+            //                            .font(.title)
+            //                            .frame(width: 200, height: 200)
+            //                            .id(item.id)
+            //                    }
+            //                }
+            //            }
             
             // List of menu items & sections
-            ScrollViewReader { test in
-                ScrollView {
-                    Button("Jump to red wines") {
-                        test.scrollTo(10, anchor: .top)
-                    }
-                    .padding()
-                    
-                    ForEach(scrollList, id: \.self) { menu in
-                        ForEach(menu, id: \.self) { sample in
-                            Section(header: Text(sample.category)) {
-                                ForEach(sample.item) { food in
-                                    Text(food.name)
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+            //            ScrollViewReader { test in
+            //                ScrollView {
+            //                    Button("Jump to red wines") {
+            //                        test.scrollTo(10, anchor: .top)
+            //                    }
+            //                    .padding()
+            //
+            //                    ForEach(scrollList, id: \.self) { menu in
+            //                        ForEach(menu, id: \.self) { sample in
+            //                            Section(header: Text(sample.category)) {
+            //                                ForEach(sample.item) { food in
+            //                                    Text(food.name)
+            //                                }
+            //                            }
+            //                        }
+            //                    }
+            //                }
+            //            }
             
             // Create a general List
-            List {
-                // Loop through the actual JSON files
-                ForEach(scrollList, id: \.self) { menu in
-                    // Loop through the Menu Categories in the JSON file
-                    ForEach(menu, id: \.self) { item in
-                        // Add the Section title per category
-                        Section(header: Text(item.category)) {
-                            // Loop through the specific menu items per category
-                            ForEach(item.item) { food in
-                                // Display the food items and modify view as needed
-                                Text(food.name)
+            
+            // TEST 2
+            ScrollViewReader { proxy in
+                VStack {
+                    Button("Jump to Red Wines") {
+                        proxy.scrollTo(4, anchor: .top)
+                    }
+                    List {
+                        // Loop through the actual JSON files
+                        ForEach(scrollList, id: \.self) { menu in
+                            // Loop through the Menu Categories in the JSON file
+                            ForEach(menu, id: \.self) { item in
+                                // Add the Section title per category
+//                                Section(header: Text(item.category).id(item.id)) {
+                                Text(item.category)
+                                    .font(.title2)
+                                    .fontWeight(.bold)
+                                    .id(item.id)
+                                    .padding()
+                                    // Loop through the specific menu items per category
+                                    ForEach(item.item) { food in
+                                        // Display the food items and modify view as needed
+                                        Text(food.name)
+                                    }
+                                }
                             }
                         }
                     }
@@ -78,7 +99,7 @@ struct ScrollViewV1: View {
             }
         }
     }
-}
+//}
 
 #Preview {
     ScrollViewV1(menu: beverages)
